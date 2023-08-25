0296e2ed9637022a4df5e854fe22d512a470e844
clxrityy <97744702+clxrityy@users.noreply.github.com>
2023-08-25 14:45:47 -0400

    meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,12 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:title" content="NEXT SPOTIFY STATS" />
+        <meta property="og:description" content="Search for an artist on Spotify" />
+        <meta property="twitter:image" content="/logo.png" />
+        <meta property="twitter:title" content="NEXT SPOTIFY STATS" />
+        <meta property="twitter:description" content="Search for an artist on Spotify" />
       </head>
 
       <body>
